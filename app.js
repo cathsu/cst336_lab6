4af60396083269ba296154c072a2b867295c89b5
EC2 Default User <ec2-user@ip-172-31-36-239.us-east-2.compute.internal>
2020-03-11 22:55:07 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// use the express module
+const express = require("express");
+
+// create an object of the express module
+const app = express(); 
+
+// use EJS to render HTML files
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static("public"))
+
+//routes
+// root route
+app.get("/", function(req, res){
+    res.render("index.html"); 
+}); 
+
+app.get("/mercury", function(req, res) {
+    res.render("mercury.html");
+}); 
+
+app.get("/venus", function(req, res) {
+    res.render("venus.html"); 
+});
+
+app.get("/earth", function(req, res) {
+    res.render("earth.html"); 
+});
+
+//server listener
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Running Express Server...");
+}); 
