0ae9f0bdcdfe69e6c7785be6acbfd21998b4ec5e
EC2 Default User <ec2-user@ip-172-31-36-239.us-east-2.compute.internal>
2020-03-12 19:26:52 +0000

    Finished lab

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ app.get("/earth", function(req, res) {
     res.render("earth.html"); 
 });
 
+app.get("/mars", function(req, res) {
+    res.render("mars.html"); 
+})
+
+app.get("*", function(req, res) {
+    res.render("error.html");
+})
 //server listener
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Running Express Server...");
